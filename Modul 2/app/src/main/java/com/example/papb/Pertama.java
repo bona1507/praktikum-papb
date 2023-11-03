319f59e470eeaca6b3d2eb53e6454420bcf5b23f
bona1507 <bnvt15@student.ub.ac.id>
2023-11-03 18:58:57 +0700

    modul 2

---
+++ Modul 2/app/src/main/java/com/example/papb/Pertama.java
@@ -0,0 +1,12 @@
+package com.example.papb;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Pertama extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kedua);
+    }
+}
